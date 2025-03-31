9b32e2559a7d1f3dc9445996f48c5f0735f0c532
liwy <liwy@huawei14s.com>
2025-03-31 20:04:46 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pytest-open-html",
     version="0.1.0",
     description="Auto-open HTML reports after pytest runs",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Your Name",
     author_email="your@email.com",
